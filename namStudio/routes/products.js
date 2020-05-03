8b2db16045410dc9fd52fa9cf9b35c95e1245b9a
naty granade <natig2003@hotmail.com>
2020-05-03 20:54:47 -0300

    Cree ruta de productos

---
+++ namStudio/routes/products.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let router = express.Router();
+let productsController = require ('../controllers/productsController.js')
+
+/* GET todos los productos. */
+router.get('/', productsController.collection);
+
+/* GET Detalle de producto. */
+router.get('/:idProduct', productsController.idProduct);
+
+module.exports = router;
