38b08b33953c2b358ea9aee69aa69383a9409d09
naty granade <natig2003@hotmail.com>
2020-05-03 14:13:54 -0300

    Creo server localhost:3000 en app.js

--- namStudio/app.js
+++ namStudio/app.js
@@ -4,10 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Requerir las rutas
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Servidor
+app.listen(3000, 'localhost')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
