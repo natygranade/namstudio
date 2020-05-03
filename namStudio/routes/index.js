19e365da39b8a61e9d80143eae8ed30b4a495ec1
naty granade <natig2003@hotmail.com>
2020-05-03 14:15:10 -0300

    Agrego controlador indexController en el router index

--- namStudio/routes/index.js
+++ namStudio/routes/index.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
+let express = require('express');
+let router = express.Router();
+let indexController = require ('../controllers/indexController.js')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index);
 
 module.exports = router;
