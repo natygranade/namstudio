8e2002c25a8c65cd8bbd933f7195ebece9e3e7b2
naty granade <natig2003@hotmail.com>
2020-05-11 18:48:07 -0300

    agrego ruta de carga de producto

--- namStudio/controllers/productsController.js
+++ namStudio/controllers/productsController.js
@@ -2,9 +2,12 @@ let productsController= {
     collection: function(req,res){
         res.render('collection')
     },
+    cargaProducto:  function(req,res){
+        res.render('cargaProducto')
+    },
     idProduct: function (req, res, next) {
         res.render('idProduct',{id:req.params.id})
-        
+    
     }
 }
 
--- namStudio/routes/products.js
+++ namStudio/routes/products.js
@@ -5,7 +5,11 @@ let productsController = require ('../controllers/productsController.js')
 /* GET todos los productos. */
 router.get('/', productsController.collection);
 
+
+router.get('/cargaProducto', productsController.cargaProducto);
+
 /* GET Detalle de producto. */
+
 router.get('/:id', productsController.idProduct);
 
 module.exports = router;
