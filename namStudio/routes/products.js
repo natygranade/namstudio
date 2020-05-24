98c0e4d0bfb5e1f9074cac101ca0f40c0e2d0a55
naty granade <natig2003@hotmail.com>
2020-05-24 14:43:57 -0300

    configuro middlware y validations del formulario de carga de producto

--- namStudio/routes/products.js
+++ namStudio/routes/products.js
@@ -1,12 +1,30 @@
 let express = require('express');
 let router = express.Router();
 let productsController = require ('../controllers/productsController.js')
+let productMiddleware = require ('../middlewares/productMiddleware')
+let productValidator = require('../middlewares/validators/productValidator')
+const multer = require ('multer')
+const path = require('path')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'data/cws')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+    }
+  })
+   
+  var upload = multer({ storage: storage })
 
 /* GET todos los productos. */
 router.get('/', productsController.collection);
 
 /* GET formulario de carga de producto */
-router.get('/cargaProducto', productsController.cargaProducto);
+router.get('/cargaProducto',  productsController.cargaProducto);
+
+/* POST  carga de producto */
+router.post('/cargaProducto', productMiddleware, productValidator, upload.any(), productsController.create);
 
 /* GET carrito de productos. */
 router.get('/carrito', productsController.carrito);
