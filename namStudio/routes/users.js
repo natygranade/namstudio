b4106e26885d4cd28537d331f1580eb5e4391bfd
naty granade <natig2003@hotmail.com>
2020-05-25 23:57:52 -0300

    agrego middlewares a las rutas login y signup

--- namStudio/routes/users.js
+++ namStudio/routes/users.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+let loginMiddleware = require ('../middlewares/signupMiddleware')
+let loginValidator = require('../middlewares/validators/signupValidator')
 let usersController = require ('../controllers/usersController.js')
+let signupMiddleware = require ('../middlewares/signupMiddleware')
+let signupValidator = require('../middlewares/validators/signupValidator')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,6 +16,9 @@ router.get('/logIn', usersController.logIn);
 
 router.get('/signUp', usersController.signUp);
 
+router.post('/signUp', signupValidator, signupMiddleware, usersController.processSignup)
 
+router.post('/logIn', loginValidator, loginMiddleware, usersController.processLogin)
 
 module.exports = router;
+
