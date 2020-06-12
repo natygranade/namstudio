f670706498e5e4a439f423ce5032af2aa1e6d5a8
naty granade <natig2003@hotmail.com>
2020-06-12 19:09:02 -0300

    agrego remember me

---
+++ namStudio/middlewares/rememberMeMiddleware.js
@@ -0,0 +1,24 @@
+const fs = require ('fs')
+const session= require('express-session')
+
+module.exports =  function rememberMeMiddleware (req,res,next){
+    console.log('here', req.session)
+    
+    if(req.cookies.rememberMe != undefined && req.session.userLoged == undefined){
+        let users ;
+        let usersFile = fs.readFileSync(path.resolve('data','users', 'users.json'), {encoding: 'utf-8'})
+        if (usersFile == ''){
+            users = []  
+        }else{
+            users = JSON.parse(usersFile)
+        }  
+        
+        let userToLog = users.find(user => user.email == req.cookies.rememberMe)
+        
+        if (userToLog) {
+            req.session.userLoged = userToLog
+         
+        }
+    }
+    next()
+} 
