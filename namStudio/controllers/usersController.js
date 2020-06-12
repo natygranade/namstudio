d6cd54a38812d2e709ed57ae53b8d3a941afdd11
naty granade <natig2003@hotmail.com>
2020-06-12 19:18:02 -0300

    configuro login y session

--- namStudio/controllers/usersController.js
+++ namStudio/controllers/usersController.js
@@ -1,16 +1,16 @@
 const fs= require ('fs')
 const path = require('path')
-const session= require('express-session')
 const bcrypt = require('bcrypt')
+const {check, validationResult, body} = require ('express-validator')
 
 let usersController= {
-    signUp: function( req, res){
-        res.render('signUp')
+    signup: function( req, res, next){
+        res.render('signup')
     },
-    logIn: function( req, res){
-        res.render('logIn')
+    login: function( req, res){
+        res.render('login')
     },
-    create:   function (req,res,next){
+    create:  function (req,res){
         let user = {
             email:req.body.email,
             name: req.body.name,
@@ -32,79 +32,89 @@ let usersController= {
         res.redirect('/')
         
     },
-    processLogin: function (req,res,next){
-        
-        let user = {
-            email:req.body.email,
-            password: req.body.password,
-            rememberMe: req.body.rememberMe
+    processLogin: function (req,res){
+        let errors = validationResult(req)
+        if (errors.isEmpty()){
+            
+            let usersFile = fs.readFileSync(path.resolve('data','users', 'users.json'), {encoding: 'utf-8'})
+            let users ;
+            if (usersFile == ''){
+                users = []  
+            }else{
+                users = JSON.parse(usersFile)
+            }  
+            let userToLog = users.find(user => user.email == req.body.email)
+            
+            if (userToLog) {
+                let valid = bcrypt.compareSync(req.body.password, userToLog.password)
+                
+                if (valid) {
+                    req.session.userLoged = userToLog.email
+                    console.log('login', req.session)
+                    return res.send('logueado '+ req.session.userLoged )
+                } else {
+                    return res.render('login', {errors: [{msg:'contrasena invalida'}]}) 
+                }
+            } 
+            
+        }else{
+            return res.render('login',{errors: errors.errors})
         }
-        let userToLog; 
-        let users;
-        let usersFile = fs.readFileSync (path.resolve('data','users', 'users.json'), {encoding: 'utf-8'})
-        if(usersFile == ''){
+        
+    },
+    check: function (req,res){
+        console.log(req.session)
+        if(req.session.userLoged){
+        res.send(req.session.userLoged)
+        } else{
+            res.send("no logueado")
+        }
+    },
+    edit: function (req,res){
+        let users;    
+        let editUser = users.findIndex(function (user) {
+            return user.id == req.params.id
+        })
+        users[editUser].email = req.body.email,
+        users[editUser].contraseña = req.body.contraseña,
+        users[editUser].nombre = req.body.nombre,
+        users[editUser].razon = req.body.razon,
+        users[editUser].provincia = req.body.provincia,
+        users[editUser].codPos = req.body.codPos,
+        users[editUser].telefono = req.body.telefono;
+        
+        
+        let usersJson = fs.readFileSync (path.resolve('data','users', 'users.json'), {encoding: 'utf-8'})
+        if(usersJson == ''){
             users = []
         } else{
-            users = JSON.parse(usersFile)
+            users = JSON.parse(usersJson)
         }
-        for (let i=0; i<users.length; i++){
-            if(users[i].email == req.body.email){
-                if(bcrypt.compareSync(req.body.password, users[i].password)){
-                    userToLog = users[i]
-                    break;
-                }
-            }
-        }
-        if(userToLog == undefined){
-            return res.render('logIn', {errors: [{msg: "Usuario no registrado"}]})
-        }
-        req.session.Loged = userToLog
-
-        res.send('usuario logeado')
+        fs.appendFileSync(path.resolve('data','users', 'users.json'),JSON.stringify(users))
+        
+        res.status(200).send('')
     },
-    edit: function (req,res,next){
-            let users;    
-            let editUser = users.findIndex(function (user) {
-                return user.id == req.params.id
-            })
-                users[editUser].email = req.body.email,
-                users[editUser].contraseña = req.body.contraseña,
-                users[editUser].nombre = req.body.nombre,
-                users[editUser].razon = req.body.razon,
-                users[editUser].provincia = req.body.provincia,
-                users[editUser].codPos = req.body.codPos,
-                users[editUser].telefono = req.body.telefono;
-            
-                      
-                let usersJson = fs.readFileSync (path.resolve('data','users', 'users.json'), {encoding: 'utf-8'})
-                if(usersJson == ''){
-                    users = []
-                } else{
-                    users = JSON.parse(usersJson)
-                }
-                fs.appendFileSync(path.resolve('data','users', 'users.json'),JSON.stringify(users))
-               
-                res.status(200).send('')
-            },
     delete: function (req,res,next){
-                
-                let index = users.findIndex(function (user) {
-                return user.id == req.params.id
-                })
-                delete users[index]
-                res.status(200).send('Ok')
-                let users;
-                let usersJson = fs.readFileSync(path.resolve('data', 'users', 'users.json'), { encoding: 'utf-8' })
-                if (usersJson == '') {
-                users = []
-                } else {
-                users = JSON.parse(usersJson)
-            }
-                fs.appendFileSync(path.resolve('data', 'users', 'users.json'), JSON.stringify(users))
-
+        
+        let index = users.findIndex(function (user) {
+            return user.id == req.params.id
+        })
+        delete users[index]
+        res.status(200).send('Ok')
+        let users;
+        let usersJson = fs.readFileSync(path.resolve('data', 'users', 'users.json'), { encoding: 'utf-8' })
+        if (usersJson == '') {
+            users = []
+        } else {
+            users = JSON.parse(usersJson)
+        }
+        fs.appendFileSync(path.resolve('data', 'users', 'users.json'), JSON.stringify(users))
+        
         res.redirect('index')
-                   
-    }
-}
+        
+    },
     
-    module.exports = usersController;
+    
+}
+
+module.exports = usersController;
