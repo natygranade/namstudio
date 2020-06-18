b93557c574759c287ea403f3af334aabd38fe9c0
apinheiro78 <apinheiro78@hotmail.com>
2020-06-18 10:31:13 -0300

    se cambian las rutas para incluir el edit

---
+++ routes/users.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+let usersController = require ('../controllers/usersController.js')
+let loginMiddleware = require ('../middlewares/loginMiddleware')
+let signupMiddleware = require('../middlewares/signupMiddleware')
+const multer = require ('multer')
+const path = require('path')
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'data/avatar')
+  },
+  filename: function (req, file, cb) {
+    console.log(file)
+    cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/signup', usersController.signup);
+
+router.post('/signup',upload.any(), signupMiddleware,  usersController.create);
+
+router.get('/login', usersController.login);
+
+router.post('/login',loginMiddleware, usersController.processLogin)
+
+router.get('/check', usersController.check)
+
+router.get('/edit/:id', usersController.edit);
+
+router.patch('/edit/:id', usersController.update);
+
+router.delete('/edit', usersController.delete);
+
+module.exports = router;
+
