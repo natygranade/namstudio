dbe804c4fda8096efbb93aa54c069a3b5e28494f
naty granade <natig2003@hotmail.com>
2020-07-12 02:35:53 -0300

    modifico la validacion del back de carga de producto

--- namStudio/middlewares/validators/productValidator.js
+++ namStudio/middlewares/validators/productValidator.js
@@ -1,9 +1,10 @@
 const {check, validationResult, body} = require ('express-validator')
 
 let productValidator = [
-    check ('name').isLength({min: 6, max:12}).withMessage('Completar nombre de diseño'),
-    check ('category').isLength().withMessage('Seeccionar categoria'),
-    check ('colorways').isLength().withMessage('Cargar 3 variantes')
+    check ('name').isLength({min:0, max: undefined}).withMessage('Completar nombre de diseño'),
+      check ('colorways').isLength({min:0, max: undefined}).withMessage('Cargar 3 variantes'),
+      check ('size').isLength({min: 0, max: 7}).withMessage('La medida debe ser alto x ancho en cm')
+    //   check ('price').isInt({min: 2, max:undefined}).withMessage('Completar precio en USD')
     ]
 
 
