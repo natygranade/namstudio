eac12e73ab83ef7a33c010119ed40e27c7c7c3f0
naty granade <natig2003@hotmail.com>
2020-07-22 22:19:54 -0300

    cargo ultimo producto de db

--- react-proyect/src/components/ProductDB.js
+++ react-proyect/src/components/ProductDB.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import product from'../images/product_dummy.svg'
+import product from'../images/colorways-1594594589930.jpg'
 
 
-function ProductDB (){
+function ProductDB (props){
+
     return (
         <div className="col-lg-6 mb-4">
         <div className="card shadow mb-4">
@@ -13,8 +14,8 @@ function ProductDB (){
                 <div className="text-center">
                     <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 25 + 'rem'}} src={product} alt=""/>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
-                <a target="_blank" rel="nofollow" href="/">View product detail</a>
+                <p>Poduct name: {props.product.name}</p>
+                <p>Category: {props.category.name} </p>
             </div>
         </div>
     </div>
