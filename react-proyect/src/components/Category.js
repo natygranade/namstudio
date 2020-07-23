c078516837023dc58945b0f0bb742164506bc4a2
naty granade <natig2003@hotmail.com>
2020-07-22 22:22:16 -0300

    agrego categorias

--- react-proyect/src/components/Category.js
+++ react-proyect/src/components/Category.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
 function Category (props){
+    
     return(
-        <div className="col-lg-6 mb-4">
-        <div className="card bg-info text-white shadow">
-            <div className="card-body">
-               {props.name}
+        <div className="row">
+        {props.name.map( (category,i) =>
+            <div key={category + 1 +i} className="col-lg-6 mb-4">
+            <div key={category + 2 +i} className="card bg-info text-white shadow">
+            
+            <div className="card-body" key={category + i}>{category} </div>
             </div>
+            </div>
+            )
+        }
+        
+        
         </div>
-    </div>
-
         )
     }
-
-export default Category
+    
+    export default Category
--- react-proyect/src/components/CategoryDB.js
+++ react-proyect/src/components/CategoryDB.js
@@ -3,64 +3,58 @@ import Category from './Category'
 
 
 class CategoryDB extends Component {
-  constructor(){
-      super()
-      this.state ={
-          categories: ""
-      }
-  }
-
-  apiCall(url, consecuencia){
-    fetch(url)
-    .then(response => response.json())
-    .then(data => consecuencia(data))
-    .catch(error => console.log(error))
-  }
-
-componentDidMount (){
-this.bringProducts()  
-}  
-
-bringProducts (){
-this.apiCall('http://localhost:8000/api/products/categories', this.showCategory)
-}
-
-showCategory = (data) => {
-
-let categoryNames = []
-for(let i=0;i<data.length;i++){
-    categoryNames.push(data[i].name)
-}
-
-this.setState({
-categories: data.name
-})
-}
-
-    render (){
-    return(
-        <div className="col-lg-6 mb-4">						
-							<div className="card shadow mb-4">
-								<div className="card-header py-3">
-									<h6 className="m-0 font-weight-bold text-primary">Categories in Data Base</h6>
-								</div>
-								<div className="card-body">
-									<div className="row">
-									<Category name="Category 01"/>
-                                    <Category name="Category 02"/>
-                                    <Category name="Category 03"/>
-                                    <Category name="Category 04"/>
-                                    <Category name="Category 05"/>
-                                    <Category name="Category 06"/>		
-											
-                                        </div>
-                                        </div>
-                                        </div>
-                                        </div>
-                                    
-
-    )
-       }
+    constructor(){
+        super()
+        this.state ={
+            categories: []
+        }
     }
-
-export default CategoryDB
+    
+    apiCall(url, consecuencia){
+        fetch(url)
+        .then(response => response.json())
+        .then(data => consecuencia(data))
+        .catch(error => console.log(error))
+    }
+    
+    componentDidMount (){
+        this.bringCategory()  
+    }  
+    
+    bringCategory (){
+        this.apiCall('http://localhost:8000/api/products/categories', this.showCategory)
+    }
+    
+    showCategory = (data) => {
+        
+        let categoryNames = []
+        for(let i=0;i<data.length;i++){
+            categoryNames.push(data[i].name)
+        }
+        this.setState({
+            categories: categoryNames
+        })
+    }
+    
+    render (){
+        let categoriesArray = this.state.categories
+        
+        return(
+            
+            <div className="col-lg-6 mb-4">						
+            <div className="card shadow mb-4">
+            <div className="card-header py-3">
+            <h6 className="m-0 font-weight-bold text-primary">Categories in Data Base</h6>
+            </div>
+            <div className="card-body">
+            
+            <Category name={categoriesArray}/>
+            </div>
+            </div>
+            </div>           
+            
+            )
+        }
+    }
+    
+    export default CategoryDB
