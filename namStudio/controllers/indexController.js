ebbf08d73c3c4d918603e3c18872c969009f3db2
apinheiro78 <apinheiro78@hotmail.com>
2020-07-26 16:25:33 -0300

    Cambio en controller index por incorporación de Contacto y Quienes Somos

--- namStudio/controllers/indexController.js
+++ namStudio/controllers/indexController.js
@@ -1,7 +1,15 @@
 let indexController= {
     index: function( req, res){
         res.render('index')
-    }
+    },
+    
+    quienes: function( req, res){
+        res.render('quienes')
+    },
+    
+    contacto: function( req, res){
+        res.render('contacto')
+    },
 }
 
 module.exports = indexController;
