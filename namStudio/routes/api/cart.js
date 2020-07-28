aa9abef5ff567306790726d548cfa1c5e45596b0
naty granade <natig2003@hotmail.com>
2020-07-27 23:59:26 -0300

    creo apis para carrito

---
+++ namStudio/routes/api/cart.js
@@ -0,0 +1,38 @@
+let express = require('express')
+let router = express.Router()
+let db = require('../../db/models')
+
+router.get ('/',  function(req,res){
+    db.Cart.findAll({
+        include: ['products'],
+        where:{
+            user_id: 2
+ //  user_id: req.session.userLoged
+           }
+}).then( response => {
+let carts = Array.from(response)
+
+        res.json(carts[carts.length-1])
+})
+
+})
+
+router.get('/products', function(req,res){
+
+    db.Cart.findAll({
+        include: ['products'],
+        where:{
+            user_id: 2,
+ //  user_id: req.session.userLoged
+        }
+
+        }).then( response => {
+            let carts = Array.from(response)
+            let lastCart = carts[carts.length-1]
+            res.json(lastCart.products)
+        })
+
+        })
+
+
+module.exports = router
