03c55302cc86b74ba40d52db3dda1b0a065b6669
naty granade <natig2003@hotmail.com>
2020-07-28 00:43:49 -0300

    agrego cantidad de productos agregados al carrito en el header

--- namStudio/public/javascripts/dashboard.js
+++ namStudio/public/javascripts/dashboard.js
@@ -4,16 +4,24 @@ window.addEventListener('load', function (){
     fetch("http://localhost:8000/api/users/logedUser")
     .then(response =>  response.json())
     .then(data => {
-        console.log(data)
-        if(data.admin){
-            let dashboard = document.getElementById('dashboard')
-            
-            dashboard.classList.remove("d-none")
-            dashboard.setAttribute("href","/dashboard")
-            
+        if(data){
+            if(data.admin){
+                let dashboard = document.getElementById('dashboard')
+                
+                dashboard.classList.remove("d-none")
+                dashboard.setAttribute("href","/dashboard")
+                
+            }
         }
     })
     
-    
-    
+    fetch("http://localhost:8000/api/cart")
+    .then(response =>  response.json())
+    .then(data => {
+        if(data){
+            let cart = document.getElementById('cart')
+            cart.innerHTML = data.products.length
+        }
+    })
 })
+
