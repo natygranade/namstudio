d8b78f0f55dc00cc93522cad1c9e0fea4101a2ea
naty granade <natig2003@hotmail.com>
2020-07-28 00:01:32 -0300

    creo validacion de front de login

--- namStudio/public/javascripts/loginValidator.js
+++ namStudio/public/javascripts/loginValidator.js
@@ -1,7 +1,21 @@
 window.addEventListener('load', function(){
-    let form = document.getElementById('form')
 
-  
-
-         
+    let form = document.getElementById('login-form')
+    
+    form.email.addEventListener('keydown', function(e){
+        e.target.classList.add("is-invalid")
+        form.submit.setAttribute("disabled", "")
+          if(e.target.value.length > 5){
+            e.target.classList.replace("is-invalid", "is-valid")
+            form.submit.removeAttribute("disabled")
+     }
+    })
+    form.password.addEventListener('keydown', function(e){
+        e.target.classList.add("is-invalid")
+        form.submit.setAttribute("disabled", "")
+        if(e.target.value.length > 3 && e.target.value.length < 9){
+            e.target.classList.replace("is-invalid", "is-valid")
+            form.submit.removeAttribute("disabled")
+     }
+    })
     })
