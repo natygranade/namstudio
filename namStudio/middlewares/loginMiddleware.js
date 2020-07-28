8d2b9389c2287b17049cd7b0d6239cf84710a702
naty granade <natig2003@hotmail.com>
2020-07-28 00:04:58 -0300

    modifico validacion de back de login y signup

--- namStudio/middlewares/loginMiddleware.js
+++ namStudio/middlewares/loginMiddleware.js
@@ -1,12 +1,12 @@
-
+const db = require('../db/models')
 const {check, validationResult, body} = require ('express-validator')
+let express = require('express');
+
 
 const loginMiddleware = [
   check ('email').isEmail().withMessage('Completar mail'),
   check ('password').isLength({min: 4, max:8}).withMessage('La contraseña debe ser de 4 a 8 caracteres')
 ]
-  
-
 
 
 module.exports = loginMiddleware
