52e1f7bbc5011820dd906018296ffae00a67c320
naty granade <natig2003@hotmail.com>
2020-07-29 23:37:30 -0300

    agrego metodo de busqueda de productos

--- namStudio/controllers/indexController.js
+++ namStudio/controllers/indexController.js
@@ -1,27 +1,53 @@
 let db = require('../db/models')
+const { Op } = require("sequelize");
 
 let indexController= {
     index: function( req, res){
         res.render('index')
     },
 
-    search: function( req, res){
-        db.Product.findAll({
+    search: async function( req, res){
+       let categories = await db.Category.findAll(
+
+        )
+let products = db.Product.findAll({
             include: ['category']
-        })
-       .then(result => {
-        let productResult= []   
-        for(let i = 0; i<result.lenght; i++) {
-            console.log(result[i].category)
-            if(result[i].category.includes(req.query.search)){
-                productResult.push(result[i])
+                  })
+      Promise.all([categories, products])
+       .then(function ([categories,products]) {
+         
+let categoryResult = ""
+let productResult = []
+        for(let i = 0; i<products.length; i++) {
+ 
+            if(products[i].detail.includes(req.query.search)){
+                 productResult.push(products[i])
             }
         }
-        
+
+        if(productResult.length === 0){
+        for(let i = 0; i<categories.length; i++) {
+ 
+            if(categories[i].name.includes(req.query.search.toUpperCase())){
+                 categoryResult = categories[i]
+                  }
+         }
+         console.log(categoryResult.id)               
+                          for(let i = 0; i<products.length; i++) {
+ 
+                            if(products[i].categoryId === categoryResult.id ){
+                                 productResult.push(products[i])
+                            }
+                        }
+                
+                        }
+
+        return  res.render('search',{products:productResult})
        })
+       .catch(error => console.log(error))
 
 
-        res.render('search')
+       
     },
 
     aboutUs: function( req, res){
