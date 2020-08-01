88eefcd59b431edda35055314eed2846b06d909c
naty granade <natig2003@hotmail.com>
2020-08-01 11:51:40 -0300

    creo ruta para mostrar productos por categoria

--- namStudio/routes/products.js
+++ namStudio/routes/products.js
@@ -7,6 +7,8 @@ let productsController = require ('../controllers/productsController')
 /* GET todos los colleccion. */
 router.get('/', productsController.collection);
 
+router.get('/category',productsController.productsByCategory)
+
 /* GET Detalle de producto. */
 router.get('/:id', productsController.idProduct);
 
