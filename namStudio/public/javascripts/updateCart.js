bc9f02d193278934b660875f505e21c31617070a
naty granade <natig2003@hotmail.com>
2020-08-01 11:45:53 -0300

    suma del precio  total de productos en carrito

--- namStudio/public/javascripts/updateCart.js
+++ namStudio/public/javascripts/updateCart.js
@@ -1,7 +1,20 @@
 window.addEventListener('load', function(){
- let price = document.getElementsByClassName('price')
- let prices = Array.from(price)
+
  let total = document.getElementById('total')
 
+
+ fetch("http://localhost:8000/api/cart/products")
+ .then(response =>  response.json())
+ .then(data => {
+
+    let amount = []
+    for(let i=0;i<data.length;i++){
+    amount.push(data[i].price)
+     }
+    let sum = amount.reduce((a,b)=> a+b)
+
+total.innerHTML = 'USD' + sum
+
+    })
 })
 
