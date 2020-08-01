07b96563f30b031254391282dda0eaf7b96a7d22
naty granade <natig2003@hotmail.com>
2020-08-01 19:55:29 -0300

    agrego al js front remover item del carrito al clickear en el boton eliminar

--- namStudio/public/javascripts/updateCart.js
+++ namStudio/public/javascripts/updateCart.js
@@ -1,20 +1,27 @@
 window.addEventListener('load', function(){
-
- let total = document.getElementById('total')
-
-
- fetch("http://localhost:8000/api/cart/products")
- .then(response =>  response.json())
- .then(data => {
-
-    let amount = []
-    for(let i=0;i<data.length;i++){
-    amount.push(data[i].price)
-     }
-    let sum = amount.reduce((a,b)=> a+b)
-
-total.innerHTML = 'USD' + sum
-
+    
+    let total = document.getElementById('total')
+    let removeButton = document.getElementsByClassName('remove')
+    
+    fetch("http://localhost:8000/api/cart/products")
+    .then(response =>  response.json())
+    .then(data => {
+        
+        let amount = []
+        for(let i=0;i<data.length;i++){
+            amount.push(data[i].price)
+        }
+        let sum = amount.reduce((a,b)=> a+b)
+        
+        total.innerHTML = 'USD' + sum
+        
     })
+    Array.from(removeButton).forEach(button =>{
+        button.addEventListener('click', function(e){
+            this.parentElement.parentElement.remove()
+        })
+    })  
+    
+    
 })
 
