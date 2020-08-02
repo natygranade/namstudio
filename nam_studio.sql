15deafbd832dbbde7a7d22ffa388d8166307763a
naty granade <natig2003@hotmail.com>
2020-08-02 12:14:39 -0300

    subo diagrama de db

--- nam_studio.sql
+++ nam_studio.sql
@@ -36,7 +36,9 @@ CREATE TABLE `carts` (
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_carts_payments1_idx` (`payment_id`),
-  CONSTRAINT `fk_carts_payments1` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `user_cart_idx` (`user_id`),
+  CONSTRAINT `fk_carts_payments1` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_cart` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=282 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -130,7 +132,6 @@ CREATE TABLE `product_cart` (
 /*!40000 ALTER TABLE `product_cart` DISABLE KEYS */;
 /*!40000 ALTER TABLE `product_cart` ENABLE KEYS */;
 
-
 --
 -- Table structure for table `products`
 --
@@ -165,7 +166,6 @@ CREATE TABLE `products` (
 INSERT INTO `products` VALUES (1,'AA21015DR','brush','colorways-1592443423956.jpg','colorways-1592443423982.jpg','colorways-1592443424043.jpg',1,'32x32',100,'2020-06-18 01:23:44','2020-06-18 01:23:44',2),(2,'AP21012SR','','colorways-1592443560838.jpg','colorways-1592443560865.jpg','colorways-1592443560875.jpg',1,'64x128',300,'2020-06-18 01:26:00','2020-07-12 19:06:47',1),(3,'AP21022JL','','colorways-1592443594380.jpg','colorways-1592443594387.jpg','colorways-1592443594392.jpg',1,'32x32',100,'2020-06-18 01:26:34','2020-06-18 01:26:34',1),(4,'TR21215JL','leaves','colorways-1594011852937.jpg','colorways-1594011852971.jpg','colorways-1594011852979.jpg',0,'64x32',200,'2020-07-06 05:04:12','2020-07-06 05:04:12',7),(5,'CON21462NG','space','colorways-1594419708036.jpg','colorways-1594419708064.jpg','colorways-1594419708072.jpg',0,'32x32',300,'2020-07-10 22:21:48','2020-07-10 22:21:48',3),(6,'FL21160DM','','colorways-1594521516732.jpg','colorways-1594521516741.jpg','colorways-1594521516750.jpg',0,'32x32',200,'2020-07-12 02:38:36','2020-07-12 02:38:36',4),(19,'CON21144MM','bananas','colorways-1594523926163.jpg','colorways-1594523926165.jpg','colorways-1594523926167.jpg',1,'32x32',150,'2020-07-12 03:18:46','2020-07-12 03:18:46',3),(20,'GE20008NG','','colorways-1594528326154.jpg','colorways-1594528326168.jpg','colorways-1594528326180.jpg',0,'64X64',150,'2020-07-12 04:32:06','2020-07-12 04:32:06',5),(21,'GE20007MC','','colorways-1594528592141.jpg','colorways-1594528592145.jpg','colorways-1594528592149.jpg',0,'32x32',100,'2020-07-12 04:36:32','2020-07-12 04:36:32',5),(22,'FL21006MC','','colorways-1594594589930.jpg','colorways-1594594589950.jpg','colorways-1594594589962.jpg',1,'32x32',250,'2020-07-12 22:56:29','2020-07-12 22:56:29',4),(23,'FL21001JL','botanico flores hojas ramas','colorways-1596308893876.jpg','colorways-1596308893887.jpg','colorways-1596308893898.jpg',1,'32x32',200,'2020-08-01 19:08:13','2020-08-01 19:08:13',4),(24,'FL21030JL','flores','colorways-1596309011816.jpg','colorways-1596309011827.jpg','colorways-1596309011835.jpg',1,'128x32',200,'2020-08-01 19:10:11','2020-08-01 19:10:11',4),(25,'ORN020002SR ','hojas ornamentos','colorways-1596309223255.jpg','colorways-1596309223259.jpg','colorways-1596309223264.jpg',0,'32x32',250,'2020-08-01 19:13:43','2020-08-01 19:13:43',6);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 
-
 --
 -- Table structure for table `users`
 --
@@ -195,7 +195,6 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (11,'Natalia Granadé','natig2003@hotmail.com','$2b$10$AyfKFI3gDeecx.yl23zWvuWp2.dvZkM64a5Im5gx4G0ka7RHNghd2','1121692170',1,'avatar-1595694042756.jpg','2020-07-25 16:20:42','2020-07-25 16:20:42','gascon','Argentina',1),(12,'alicia manevy','alimanevy@hotmail.com','$2b$10$JvkW9DmhQ2DmeeckeaW0O.p4BKv/EUm.G8FgJhZKIK8VX7jxwgF0.','0',1,'','2020-07-25 20:04:49','2020-07-25 20:04:49','','',0),(13,'pepe  ','pepe@gmail.com','$2b$10$2wzY4FN6WVC0SoWFfBTJ7OrnGXgAoTYVlSW8Lkuh4KZEjoIc.vvMW','',1,'','2020-07-30 22:44:47','2020-07-30 22:44:47','','',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
@@ -217,4 +216,4 @@ INSERT INTO `users` VALUES (11,'Natalia Granadé','natig2003@hotmail.com','$2b$1
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-01 20:04:24
+-- Dump completed on 2020-08-02 11:50:29
